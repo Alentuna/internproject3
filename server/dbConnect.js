983a3e3843cd6908c512b10eeab488c95d9020f6
Alentuna <93052252+Alentuna@users.noreply.github.com>
2024-10-16 20:35:25 +0530

    Update dbConnect.js

--- server/dbConnect.js
+++ server/dbConnect.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 module.exports = async () => {
     const mongoUri =
-        "mongodb+srv://anuj:Ya0fKBalld60gpfs@cluster0.rblpaxc.mongodb.net/?retryWrites=true&w=majority";
+        "mongodb+srv://arihant:fP67I74Tvl8pLtS2@cluster0.wjn40.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
     try {
         const connect = await mongoose.connect(mongoUri, {
